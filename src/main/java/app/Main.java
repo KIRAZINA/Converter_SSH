304d4599c28cce1d377dc68beafe26ddab1d6fa0
KIRAZINA <zinman323@gmail.com>
2025-12-18 18:26:12 +0200

    feat: add miles to kilometers conversion

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,9 +1,19 @@
 package app;
 
 public class Main {
+    private static final double MILES_TO_KM = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("Converter App - Miles/Kilometers");
         System.out.println("Version: 1.0");
         System.out.println("Author: Kirilo");
+
+        double miles = 10;
+        double km = convertMilesToKm(miles);
+        System.out.println(miles + " miles = " + km + " km");
+    }
+
+    private static double convertMilesToKm(double miles) {
+        return miles * MILES_TO_KM;
     }
 }
