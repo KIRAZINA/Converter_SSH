b76c2e27a1aad4ca790bcce1b2649e90b787036c
KIRAZINA <zinman323@gmail.com>
2025-12-18 18:20:42 +0200

    Initial commit: project setup and basic info output

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Converter App - Miles/Kilometers");
+        System.out.println("Version: 1.0");
+        System.out.println("Author: Kirilo");
+    }
+}
